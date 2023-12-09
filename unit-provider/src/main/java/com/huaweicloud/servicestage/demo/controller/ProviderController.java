835152278f7ac2a56e016b7b7fbeee29fa42152e
provenceee <83857838+provenceee@users.noreply.github.com>
2023-12-09 18:39:20 +0800

    增加故障模拟

--- unit-provider/src/main/java/com/huaweicloud/servicestage/demo/controller/ProviderController.java
+++ unit-provider/src/main/java/com/huaweicloud/servicestage/demo/controller/ProviderController.java
@@ -2,6 +2,7 @@ package com.huaweicloud.servicestage.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -27,13 +28,19 @@ public class ProviderController {
     @Value("${spring.cloud.servicecomb.discovery.datacenter.availableZone}")
     private String availableZone;
 
+    private boolean ex;
+
     /**
      * 测试方法
      *
      * @return msg
+     * @throws Exception ex
      */
     @GetMapping("unit-provider/hello")
-    public Map<String, Object> hello() {
+    public Map<String, Object> hello() throws Exception {
+        if (ex) {
+            throw new Exception("exception");
+        }
         Map<String, String> msg = new HashMap<>();
         msg.put("datacenterName", datacenterName);
         msg.put("region", region);
@@ -41,5 +48,13 @@ public class ProviderController {
         Map<String, Object> map = new HashMap<>();
         map.put(name, msg);
         return map;
+    }
+
+    /**
+     * 测试方法
+     */
+    @GetMapping("unit-provider/ex")
+    public void ex(@RequestParam("enabled") boolean enabled) {
+        ex = enabled;
     }
 }
