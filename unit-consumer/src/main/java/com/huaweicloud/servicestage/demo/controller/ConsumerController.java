9e3327785a876bccb1ec9f6c9dc90fe8e0140595
provenceee <83857838+provenceee@users.noreply.github.com>
2024-02-01 16:41:41 +0800

    链路追踪demo

--- unit-consumer/src/main/java/com/huaweicloud/servicestage/demo/controller/ConsumerController.java
+++ unit-consumer/src/main/java/com/huaweicloud/servicestage/demo/controller/ConsumerController.java
@@ -3,14 +3,8 @@ package com.huaweicloud.servicestage.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.LocalDateTime;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.PostConstruct;
 
@@ -27,51 +21,24 @@ public class ConsumerController {
     @Value("${request.ip:127.0.0.1:8092}")
     private String ip;
 
+    @Value("${request.path:/unit-provider/hello}")
+    private String path;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @PostConstruct
     public void init() {
-        PROVIDER_URL = "http://" + ip + "/unit-provider/hello";
+        PROVIDER_URL = "http://" + ip + path;
     }
 
     /**
      * 测试方法
      *
-     * @param times 持续访问时间
      * @return msg
-     * @throws InterruptedException ex
      */
-    @GetMapping("unit-consumer/retry")
-    public String retry(@RequestParam(value = "times", defaultValue = "60") int times) throws InterruptedException {
-        StringBuffer sb = new StringBuffer();
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        AtomicInteger timer = new AtomicInteger();
-        executorService.execute(() -> {
-            int count = 1;
-            while (timer.get() < times) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    String result = restTemplate.getForObject(PROVIDER_URL, String.class);
-                    sb.append(count).append("|").append(LocalDateTime.now()).append("|info|").append(result);
-                } catch (Exception e) {
-                    sb.append(count).append("|").append(LocalDateTime.now()).append("|error|").append(e.getMessage());
-                }
-                sb.append(System.lineSeparator());
-                count++;
-            }
-        });
-        while (timer.getAndIncrement() < times) {
-            Thread.sleep(1000);
-        }
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {
-            Thread.sleep(100);
-        }
-        return sb.toString();
+    @GetMapping("unit-consumer/hello")
+    public String hello() {
+        return restTemplate.getForObject(PROVIDER_URL, String.class);
     }
 }
